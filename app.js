a6b7066440d38a8651efcfb3a59f1009792eb1fd
Haleemat Adetoro <haleematadetoro24@gmail.com>
2023-03-17 13:46:01 -0500

    refactoring

--- app.js
+++ app.js
@@ -4,85 +4,83 @@ const http = require("http");
 const server = http.createServer(app);
 
 const session = require("express-session");
-// const MemoryStore = session.MemoryStore;
 const { Server } = require("socket.io");
 const io = new Server(server);
-// const store = new MemoryStore();
 
 // Use session middleware
-app.use(session({
-  secret: "mysecretKey",
+const sessionMiddleware = session({
+  secret: "secret-key",
   resave: false,
   saveUninitialized: true,
   cookie: {
     secure: false,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
-}));
+});
 
 // Serve static files from the public directory
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 const items = {
-  11: "Roasted Chicken",
-  12: "Chicken Burger",
+  11: "Coleslaw",
+  12: "Roasted chicken",
   13: "Chicken Pizza",
-  14: "Pepperoni Pizza",
-  15: "French Fries",
+  14: "Chicken Burger",
+  15: "salad",
 };
 
 const orderHistory = [];
 
-// io.use((socket, next) => {
-//   sessionMiddleware(socket.request, socket.request.res, next);
-// });
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, socket.request.res, next);
+});
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   // Get the unique identifier for the user's device
-  const userDeviceId = socket.handshake.headers["user-agent"];
+  const deviceId = socket.handshake.headers["user-agent"];
 
   // Check if the user already has an existing session
   if (
-    socket.request.session[userDeviceId] &&
-    socket.request.session[userDeviceId].userName
+    socket.request.session[deviceId] &&
+    socket.request.session[deviceId].userName
   ) {
     // If the user already has a session, use the existing user name and current order
     socket.emit(
       "bot-message",
       `Welcome back, ${
-        socket.request.session[userDeviceId].userName
+        socket.request.session[deviceId].userName
       }! You have a current order of ${socket.request.session[
-        userDeviceId
+        deviceId
       ].currentOrder.join(", ")}`
     );
   } else {
     // If the user does not have a session, create a new session for the user's device
-    socket.request.session[userDeviceId] = {
+    socket.request.session[deviceId] = {
       userName: "",
       currentOrder: [],
-      userDeviceId: userDeviceId, // store the userDeviceId in the session object
+      deviceId: deviceId, // store the deviceId in the session object
     };
   }
 
   // Ask for the user's name if not provided already
-  if (!socket.request.session[userDeviceId].userName) {
-    socket.emit("bot-message", "Hello! Please what's your name?");
+  if (!socket.request.session[deviceId].userName) {
+    socket.emit("bot-message", "Hi! What's your name?");
   } else {
     socket.emit(
       "bot-message",
       `Welcome back, ${
-        socket.request.session[userDeviceId].userName
+        socket.request.session[deviceId].userName
       }! You have a current order of ${socket.request.session[
-        userDeviceId
+        deviceId
       ].currentOrder.join(", ")}`
     );
   }
 
-  let userName = socket.request.session[userDeviceId].userName;
+  let userName = socket.request.session[deviceId].userName;
 
   // Listen for incoming bot messages
   socket.on("bot-message", (message) => {
@@ -97,28 +95,28 @@ io.on("connection", (socket) => {
     if (!userName) {
       // Save the user's name and update the welcome message
       userName = message;
-      socket.request.session[userDeviceId].userName = userName;
+      socket.request.session[deviceId].userName = userName;
       socket.emit(
         "bot-message",
-        `Welcome to the Lemma's Restaurant ChatBot, ${userName}!\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
+        `Welcome to the Lemma's ChatBot, ${userName}! select\n1. To place an order\n99. To checkout order\n98. To view order history\n97. To view current order\n0. To cancel order`
       );
     } else {
       switch (message) {
         case "1":
-          // Generate the list of items dynamically
+          // Generate the list of items on the menu
           const itemOptions = Object.keys(items)
             .map((item) => `${item}. ${items[item]}`)
             .join("\n");
           socket.emit(
             "bot-message",
-            `The menu items are:\n${itemOptions}\nType the item number to add to your order`
+            `The Items on our menu are:\n${itemOptions}\nSelect the item number to add to your order`
           );
           break;
         case "97":
-          // Show the user their current order
-          if (socket.request.session[userDeviceId].currentOrder.length > 0) {
+          // Show the user's their current order
+          if (socket.request.session[deviceId].currentOrder.length > 0) {
             const currentOrder =
-              socket.request.session[userDeviceId].currentOrder.join(", ");
+              socket.request.session[deviceId].currentOrder.join(", ");
             socket.emit(
               "bot-message",
               `Your current order: ${currentOrder}\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
@@ -132,9 +130,9 @@ io.on("connection", (socket) => {
           break;
         case "99":
           // Checkout the order
-          if (socket.request.session[userDeviceId].currentOrder.length > 0) {
+          if (socket.request.session[deviceId].currentOrder.length > 0) {
             const currentOrder =
-              socket.request.session[userDeviceId].currentOrder.join(", ");
+              socket.request.session[deviceId].currentOrder.join(", ");
             orderHistory.push({
               user: userName,
               order: currentOrder,
@@ -144,7 +142,7 @@ io.on("connection", (socket) => {
               "bot-message",
               `Thanks for your order, ${userName}! Your order of ${currentOrder} will be ready shortly.\n1. Place an order\n98. Order history\n0. Cancel order`
             );
-            socket.request.session[userDeviceId].currentOrder = [];
+            socket.request.session[deviceId].currentOrder = [];
           } else {
             socket.emit(
               "bot-message",
@@ -165,29 +163,29 @@ io.on("connection", (socket) => {
               .join("\n");
             socket.emit(
               "bot-message",
-              `Here is the order history:\n${history}\n1. Place an order\n98. Order history\n0. Cancel order`
+              `Here is your order history:\n${history}. select \n1. To place an order\n0. To cancel order`
             );
           } else {
             socket.emit(
               "bot-message",
-              `There is no order history yet. Type '1' to see the menu.`
+              `You have no order history. Type '1' to see the menu.`
             );
           }
           break;
         case "0":
           // Cancel the order
-          const currentOrder = socket.request.session[userDeviceId].currentOrder;
+          const currentOrder = socket.request.session[deviceId].currentOrder;
           if (currentOrder.length === 0 && orderHistory.length === 0) {
             socket.emit(
               "bot-message",
               `There is nothing to cancel. Type '1' to see the menu.`
             );
           } else {
-            socket.request.session[userDeviceId].currentOrder = [];
+            socket.request.session[deviceId].currentOrder = [];
             orderHistory.length = 0;
             socket.emit(
               "bot-message",
-              `Your order has been cancelled.\n1. Place a new order\n98. Order history`
+              `Your order has been cancelled. Select \n1. To place a new order\n98. To view order history`
             );
           }
           break;
@@ -195,17 +193,17 @@ io.on("connection", (socket) => {
           // Add the item to the current order
           const itemNumber = parseInt(message);
           if (!isNaN(itemNumber) && items[itemNumber]) {
-            socket.request.session[userDeviceId].currentOrder.push(
+            socket.request.session[deviceId].currentOrder.push(
               items[itemNumber]
             );
             socket.emit(
               "bot-message",
-              `You have added ${items[itemNumber]} to your current order\n Add another order from the menu\n Type '97' to see your current order\n '98' to see order history\n '99' to checkout\n '0' to cancel your order`
+              `You have added ${items[itemNumber]} to your current order\n you can order something else from the menu\n Type '97' to see your current order\n '98' to see order history\n '99' to checkout\n '0' to cancel your order`
             );
           } else {
             socket.emit(
               "bot-message",
-              `Invalid input. Type '1' to see the menu.`
+              `Invalid. Type '1' to see the menu.`
             );
           }
           break;
@@ -214,7 +212,7 @@ io.on("connection", (socket) => {
   });
   // Listen for disconnection event
   socket.on("disconnect", () => {
-    delete socket.request.session[userDeviceId];
+    delete socket.request.session[deviceId];
 
     console.log("User disconnected:", socket.id);
   });

b2155c7d04cb664d22dc97ec88306d9bef783d1f
Haleemat Adetoro <haleematadetoro24@gmail.com>
2023-03-17 15:08:29 -0500

    refactoring

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000
+
 const session = require("express-session");
 const { Server } = require("socket.io");
 const io = new Server(server);
-require('dotenv').config()
+
 
 // Use session middleware
 const sessionMiddleware = session({
@@ -219,6 +219,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`listening on port: ${PORT}`);
+server.listen(3000, () => {
+  console.log(`listening on port: 3000`);
 });
